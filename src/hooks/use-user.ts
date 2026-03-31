d7df47beb5d0476ad1d8aae808dc44f155aeb6c7
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2026-03-31 15:23:54 -0300

    feat: function createUser with POST

--- src/hooks/use-user.ts
+++ src/hooks/use-user.ts
@@ -1,5 +1,5 @@
 import React from 'react';
-import { fetcher } from '../helpers/api';
+import { api, fetcher } from '../helpers/api';
 import type { User } from '../models/user';
 
 export default function useUser() {
@@ -21,9 +21,23 @@ export default function useUser() {
     }
   }, []);
 
+  async function createUser(payload: User) {
+    try {
+      setRequestStatus('saving');
+      await api('/users', { method: 'POST', body: JSON.stringify(payload) });
+      alert('Usuário criado com sucesso!');
+    } catch (event) {
+      console.error(event);
+      alert('Erro ao criar usuário');
+    } finally {
+      setRequestStatus('idle');
+    }
+  }
+
   return {
     user,
     userRequestStatus: requestStatus,
     getUser,
+    createUser,
   };
 }
